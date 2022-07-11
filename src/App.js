e7a8b013bcc358d70991516dd61e81dd0428e4ba
Yohanesbene <yohanes.benedictus19@gmail.com>
2022-07-11 23:19:44 +0700

    on going todolist ui, form ui done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Container, Form, Button, Stack, Card } from "react-bootstrap"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <h1 className="text-center mt-5">Todo Web App</h1>
+
+      <Card className="mt-5">
+        <Card.Body>
+          <Stack direction="horizontal" gap={3}>
+            <Form.Control type="text" placeholder="Masukkan Todo..." />
+            <Button>Add</Button>
+          </Stack>
+        </Card.Body>
+      </Card>
+
+
+    </Container>
+  )
 }
 
-export default App;
+export default App
